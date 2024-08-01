a30796207822e0c911690176eec6dbe7a000747d
ashutosh0640 <ashulook1013@gmail.com>
2024-08-01 12:30:05 +0530

    typing master

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -9,21 +9,45 @@ function App() {
   const [count, setCount] = useState(0)
 
   var MinusOne = () => {
-    console.log("minus one")
+    //console.log("minus one")
     setCount(count - 1)
   }
 
   var PlusOne = () => {
-    console.log("plus one")
+    //console.log("plus one")
     setCount(count + 1)
   }
+
+  const [text, setText] = useState('')
+  document.getElementById('content').innerText = text;
+
+
   return (
     <>
-      <h1>Counter App</h1>
-      <div id='counter'>
-        <Minus onClick={MinusOne}/>
-        <h3>Count: {count}</h3>
-        <Plus onClick={PlusOne}/>
+      {/* Main div */}
+      <div className=' main_div'>
+
+
+        {/*Left Child Div */}
+        <div className=' '>
+          <h1 className=' '>Typing Master</h1>
+          <div id='content'></div>
+          <input className=' ' type="text" onChange={(e) => setText(e.target.value)} />
+        </div>
+
+
+        {/*Right Child Div */}
+        <div className=' main_div_child2'>
+          <h1>Counter App</h1>
+          <div id='counter'>
+            <Minus onClick={MinusOne} />
+            <h3>Count: {count}</h3>
+            <Plus onClick={PlusOne} />
+          </div>
+        </div>
+
+
+
       </div>
     </>
   )
